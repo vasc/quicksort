6a9cb541eb3e4ca2015b2a37d28b1cda53572c59
Vasco Fernandes <vasco.box@gmail.com>
2011-11-25 07:17:33 +0000

    python

---
+++ quicksort.py
@@ -0,0 +1,13 @@
+import sys
+
+def quicksort(l):
+	if not l: return l
+	pivot = l[0]
+	smaller = [x for x in l[1:] if x < pivot]
+	larger = [x for x in l[1:] if x >= pivot]
+	return quicksort(smaller) + [pivot] + quicksort(larger)
+
+l = map(int, sys.argv[1:])
+
+print " ".join(map(str, quicksort(l)))
+
